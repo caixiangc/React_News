ab3bd669f788330c5b886bdaf54475a142bc1f8b
caixiangc <939387484@qq.com>
2017-12-21 21:31:38 +0800

    first commit

---
+++ src/js/components/testindex.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MyselfDesign from './myselfdesign';
+export default class TestIndex extends React.Component {
+	render() {
+		return (
+			<div>
+				<MyselfDesign></MyselfDesign>
+			</div>
+		);
+	};
+}
